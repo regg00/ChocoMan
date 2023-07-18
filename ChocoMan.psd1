b7695552ade698cc581bbddbea98b7dd81fda99c
Régis Tremblay Lefrançois <regg00@gmail.com>
2023-07-18 08:50:55 -0400

    added Get-ChocoOutdated

--- ChocoMan.psd1
+++ ChocoMan.psd1
@@ -39,7 +39,8 @@
     FunctionsToExport = @(
         'Get-ChocoVersion',
         'Get-ChocoPackages',
-        'Get-ChocoPackage'
+        'Get-ChocoPackage',
+        'Get-ChocoOutdated'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ Public/Get/Get-ChocoOutdated.ps1
@@ -0,0 +1,20 @@
+Function Get-ChocoOutdated {
+    <#
+    .SYNOPSIS
+        Get the list of outdated chocolatey packages.
+    .DESCRIPTION    
+        Get the list of outdated chocolatey packages.
+    .OUTPUTS
+        PSCustomObject
+    #>
+    [CmdletBinding()]
+    [OutputType([String])]
+    param()
+
+    if (Test-ChocoInstalled) {
+        $Header = "Name", "CurrentVersion", "AvailableVersion", "Pinned"
+        $ChocoPackages = ConvertFrom-Csv (choco outdated -r -nocolor) -Delimiter '|' -Header $Header
+        Return $ChocoPackages
+    }
+
+}
