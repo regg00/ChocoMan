d44c47580756318a25ee9dd3791d5878f98344ea
Régis Tremblay Lefrançois <regg00@gmail.com>
2023-07-18 09:07:40 -0400

    added Get-ChocoSources

--- ChocoMan.psd1
+++ ChocoMan.psd1
@@ -40,7 +40,8 @@
         'Get-ChocoVersion',
         'Get-ChocoPackages',
         'Get-ChocoPackage',
-        'Get-ChocoOutdated'
+        'Get-ChocoOutdated',
+        'Get-ChocoSources'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ Public/Get/Get-ChocoSources.ps1
@@ -0,0 +1,19 @@
+Function Get-ChocoSources {
+    <#
+    .SYNOPSIS
+        Get the list of chocolatey sources.
+    .DESCRIPTION
+        Get the list of chocolatey sources.
+    .OUTPUTS
+        PSCustomObject
+    #>
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    param()
+    
+    if (Test-ChocoInstalled) {
+        $Header = "Name", "Uri", "Unknown0", "UserName", "Unknown1", "BypassProxy", "SelfService", "AdminOnly"
+        $ChocoPackages = ConvertFrom-Csv (choco source list -r -nocolor) -Delimiter '|' -Header $Header
+        Return $ChocoPackages | Select-Object Name, Uri, Username, BypassProxy, SelfService, AdminOnly
+    }
+}
