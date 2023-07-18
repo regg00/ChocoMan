7793bb9a97b5f26ac58dd3ad745599d3ac71a6c6
Régis Tremblay Lefrançois <regg00@gmail.com>
2023-07-18 13:26:50 -0400

    removed alias

--- Public/Install/Install-Choco.ps1
+++ Public/Install/Install-Choco.ps1
@@ -34,7 +34,7 @@ Function Install-Choco {
     )
 
     if (!(Test-ChocoInstalled)) {        
-        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString($InstallerUrl))        
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($InstallerUrl))        
         return "Chocolatey has been installed."
     }
     else {
