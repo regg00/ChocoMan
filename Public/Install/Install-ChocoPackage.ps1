ab94092972e9b00cd6c0a69a5d094a665da47b5d
regg00 <regg00@gmail.com>
2023-07-19 13:45:50 -0400

    upgrade now works

--- Public/Install/Install-ChocoPackage.ps1
+++ Public/Install/Install-ChocoPackage.ps1
@@ -1,20 +1,25 @@
 Function Install-ChocoPackage {
     <#
     .SYNOPSIS
-        Installs a chocolatey package. Just like Chocolatey, you may need admin rights to install a package.
+        Installs a chocolatey package. Doesn't asks for confirmation by default. Just like Chocolatey, you may need admin rights to install a package.
     .DESCRIPTION    
-        Installs a chocolatey package. Just like Chocolatey, you may need admin rights to install a package.
+        Installs a chocolatey package. Doesn't asks for confirmation by default. Just like Chocolatey, you may need admin rights to install a package.
     .PARAMETER Name
         The name of the package to install.
     .PARAMETER Source
         The source to install the package from.
     .PARAMETER Upgrade
-        If the package should be upgraded if it already exists.
-    .PARAMETER Confirm
-        If the package should be installed with confirmation.
+        If the package should be upgraded if it already exists.    
+    .PARAMETER Force
+        Will force the reinstallation of the package.
+    .PARAMETER
+        Ask for confirmation before installing the package.
     .EXAMPLE
         Install-ChocoPackage -Name vlc
-        
+    .EXAMPLE
+        Install-ChocoPackage -Name vlc -Source chocolatey
+    .EXAMPLE
+        Install-ChocoPackage -Name vlc -Source chocolatey -Upgrade
 
     .OUTPUTS
         String
@@ -25,22 +30,31 @@ Function Install-ChocoPackage {
         [Parameter(Mandatory = $true, Position = 0)]
         [String] $Name,
         [String] $Source = "chocolatey",
-        [Switch] $Upgrade = $false
+        [Switch] $Upgrade = $false,
+        [Switch] $Force = $false,
+        [Switch] $AskForConfirmation = $false
     )
 
     if (Test-ChocoInstalled) {
+        $Command = "install"
         if ($Upgrade) {
             $Command = "upgrade"
         }
-        else {
-            $Command = "install"
+        elseif ($Force) {
+            $Command = "install --force"
         }
-        if ($PSCmdlet.ShouldProcess($Name)) {
+        
+        if ($AskForConfirmation) {
             choco $Command $Name --source $Source
+            Write-Verbose "choco $Command $Name --source $Source"
+            
         }
         else {
-            choco $Command -y $Name --source $Source
-        }                
+            choco $Command $Name -y --source $Source            
+            Write-Verbose "choco $Command $Name -y --source $Source"
+        }
+        
+                       
     }
     else {
         Write-Error "Chocolatey is not installed. Please install it first."
