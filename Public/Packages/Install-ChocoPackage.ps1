ff721fc092827f889ce3622ffd5ec2e7fce87290
Michael Wenders <1871010+we-mi@users.noreply.github.com>
2023-07-31 20:44:00 +0200

    (#15) Implemented passing multiple package names to "Install-ChocoPackage"
    Also adapted basic code structure of "Uninstall-ChocoPackage":
    - Implemented "AskForConfirmation"
    - Adapted the way the Parameters are saved to $Arguments
    - Implemented pipeline input for package names

--- Public/Packages/Install-ChocoPackage.ps1
+++ Public/Packages/Install-ChocoPackage.ps1
@@ -7,7 +7,7 @@ Function Install-ChocoPackage {
         Installs a chocolatey package. Doesn't asks for confirmation by default. Just like Chocolatey, you may need admin rights to install a package.
 
     .PARAMETER Name
-        The name of the package to install.
+        The name of the package to install. You can specify more than one package.
 
     .PARAMETER Source
         The source to install the package from.
@@ -17,6 +17,9 @@ Function Install-ChocoPackage {
 
     .PARAMETER Force
         Will force the reinstallation of the package.
+
+    .PARAMETER AskForConfirmation
+        Ask for confirmation before uninstalling the package.
 
     .EXAMPLE
         Install-ChocoPackage -Name vlc
@@ -30,49 +33,68 @@ Function Install-ChocoPackage {
     #>
     [OutputType([PSCustomObject])]
     param(
-        [Parameter(Mandatory = $true, Position = 0)]
-        [String] $Name,
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true)]
+        [String[]] $Name,
         [String] $Source = "chocolatey",
         [Switch] $Upgrade = $false,
-        [Switch] $Force = $false
+        [Switch] $Force = $false,
+        [Switch] $AskForConfirmation
+
     )
 
-    if (Test-ChocoInstalled) {
+    begin {
+        if (-Not (Test-ChocoInstalled)) {
+            Write-Error "Chocolatey is not installed. Please install it first."
+            return
+        }
 
         if ($Upgrade) {
-            [String[]]$Arguments += "upgrade", "-y"
-        }
-        elseif ($Force) {
-            [String[]]$Arguments += "install", "--force", "-y"
-        }
-        else {
-            [String[]]$Arguments += "install", "-y"
+            [String[]]$Arguments = "upgrade"
+        } else {
+            [String[]]$Arguments = "install"
         }
 
-        $Arguments += $Name, "--source", $Source
-
-        $CommandOutput = Invoke-ChocoCommand $Arguments
-        if ($CommandOutput.Status -eq "Success") {
-            if ($CommandOutput.RawOutput -like "*already installed.*") {
-
-                $Status = "Already installed"
-
-            }
-            elseif ($CommandOutput.RawOutput -like "*The install of $Name was successful*") {
-                $Status = "Installed"
-
-            }
-            $Version = (Get-ChocoPackage $Name).Version
+        if ($Force) {
+            $Arguments += "--force"
         }
 
-        Return [PSCustomObject]@{
-            Name    = $Name
-            Status  = $Status
-            Version = $Version
+        if (-Not ($AskForConfirmation)) {
+            $Arguments += "-y"
         }
-
     }
-    else {
-        Write-Error "Chocolatey is not installed. Please install it first."
+
+    process {
+
+        $Arguments += "--source", $Source
+
+        foreach ($package in $Name) {
+
+            $CommandOutput = Invoke-ChocoCommand ($Arguments + $package)
+
+            if ($CommandOutput.Status -eq "Success") {
+                if ($CommandOutput.RawOutput -like "*already installed.*") {
+
+                    $Status = "Already installed"
+
+                }
+                elseif ($CommandOutput.RawOutput -like "*The install of $package was successful*") {
+                    $Status = "Installed"
+
+                }
+                $Version = (Get-ChocoPackage $package).Version
+            } else {
+                $Status = "Error"
+            }
+
+            [PSCustomObject]@{
+                Name   = $package
+                Status = $Status
+                Version = $Version
+            }
+        }
+    }
+
+    end {
+
     }
 }
